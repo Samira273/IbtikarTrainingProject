1eb06e6fbbbc2df6c961f47a3c9dd1ef9c2f63c5
Samira273 <samiramarassy@gmail.com>
2019-08-28 12:47:39 +0200

    ok-http commit

--- app/src/main/java/com/example/ibtikarproject/MainActivity.java
+++ app/src/main/java/com/example/ibtikarproject/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.ibtikarproject;
 
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -17,17 +21,25 @@ import java.util.concurrent.ExecutionException;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView=(TextView)findViewById(R.id.txt_view);
+        final TextView textView = (TextView) findViewById(R.id.txt_view);
+        final Button okHttpButton = findViewById(R.id.okhttp);
+
 
         @SuppressLint("StaticFieldLeak") final AsyncTask myTask = new AsyncTask() {
             String inputFromApi;
+
             @Override
             protected Object doInBackground(Object[] objects) {
                 URL url = null;
@@ -49,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 try {
                     InputStream in = urlConnection.getInputStream();
-                    inputFromApi= readStream(in);
+                    inputFromApi = readStream(in);
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -70,22 +82,70 @@ public class MainActivity extends AppCompatActivity {
         };
 
         myTask.execute();
-        try {
-            Log.i("gabelmsg",myTask.get().toString());
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+
+
+
+        okHttpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                @SuppressLint("StaticFieldLeak") final AsyncTask taskTwo=new AsyncTask() {
+
+                    String response = null;
+                    @Override
+                    protected Object doInBackground(Object[] objects) {
+
+                        OkHttpClass example = new OkHttpClass();
+
+                        try {
+                            response = example.run("https://api.themoviedb.org/3/movie/popular?api_key=6b93b25da5cdb9298216703c40a31832");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return response;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Object o) {
+                        super.onPostExecute(o);
+                        TextView textView1=findViewById(R.id.textView);
+                        textView1.setText(response);
+                    }
+                };
+                taskTwo.execute();
+            }
+        });
+
     }
 
     private String readStream(InputStream is) throws IOException {
         StringBuilder sb = new StringBuilder();
-        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
-        for (String line = r.readLine(); line != null; line =r.readLine()){
+        BufferedReader r = new BufferedReader(new InputStreamReader(is), 1000);
+        for (String line = r.readLine(); line != null; line = r.readLine()) {
             sb.append(line);
         }
         is.close();
         return sb.toString();
+    }
+
+
+
+    public class OkHttpClass {
+        OkHttpClient client = new OkHttpClient();
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        String run(String url) throws IOException {
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+
+            try (Response response = client.newCall(request).execute()) {
+                return response.body().string();
+            }
+        }
     }
 }
